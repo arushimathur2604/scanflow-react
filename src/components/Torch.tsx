e0c5b9aa20f00f78ef4d9f2cc059e81c08a8283f
Arushi Mathur <mathurarushi4@gmail.com>
2025-09-16 16:09:38 +0530

    Initial commit: React QR Scanner project

---
+++ src/components/Torch.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import TorchOnIcon from '../assets/TorchOnIcon';
+import TorchOffIcon from '../assets/TorchOffIcon';
+
+interface ITorchProps {
+    scanning: boolean;
+    status: boolean;
+    torchToggle: (value: boolean) => void;
+}
+
+export default function Torch(props: ITorchProps) {
+    const { status, scanning, torchToggle } = props;
+
+    function toggleTorch(value: boolean) {
+        torchToggle(value);
+    }
+
+    if (!scanning || !torchToggle) {
+        return null;
+    }
+
+    return (
+        <div style={{ bottom: 35, right: 8, position: 'absolute', zIndex: 2, cursor: 'pointer' }}>
+            {status ? <TorchOffIcon onClick={() => toggleTorch(false)} /> : <TorchOnIcon onClick={() => toggleTorch(true)} />}
+        </div>
+    );
+}
---
+++ src/misc/defaultComponents.ts
@@ -0,0 +1,9 @@
+import { IScannerComponents } from '../types';
+
+export const defaultComponents: IScannerComponents = {
+    finder: true,
+    torch: true,
+    tracker: undefined,
+    onOff: false,
+    zoom: false
+};
